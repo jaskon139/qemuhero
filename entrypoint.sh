db3c7638ddfaafc9fa47bbe51545ca3431169742
jaskon139 <jaskon139@gmail.com>
2019-05-11 15:10:20 +0800

    sdfs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-qemu-system-i386 -nographic -hda /app/openwrt-x86-generic-combined-ext4.img< /dev/null &
+qemu-system-i386 -nographic -hda /app/openwrt-x86-generic-combined-ext4.img > /tmp/log.txt 2>&1 &
 
 /usr/local/bin/gotty --port $PORT -c user:pass --permit-write --reconnect /bin/bash 
