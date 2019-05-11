28a63a2f3cbe751c00c6cdbbba938afae5f2ff93
jaskon139 <jaskon139@gmail.com>
2019-05-11 13:27:13 +0800

    sdfsdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp:127.0.0.1:8888-:22 -m 512 -hda /app/openwrt-x86-generic-combined-ext4.img< /dev/null &
+qemu-system-i386 -nographic -net user,hostfwd=tcp:127.0.0.1:8888-:22 -m 512 -hda /app/openwrt-x86-generic-combined-ext4.img< /dev/null &
 
 /usr/local/bin/gotty --port $PORT -c user:pass --permit-write --reconnect /bin/bash 
